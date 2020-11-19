e41aab88198ee805214f6eb43b0010413019a823
Emmanuelcookey15 <Emmanuelcookey744@gmail.com@users.noreply.github.com>
2020-11-19 01:35:50 +0100

    final project version

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade900,
+            title: Text('Ask Me Anything'),
+          ),
+          backgroundColor: Colors.blue,
+          body: BallActivity(),
+        ),
       ),
     );
+
+class BallActivity extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallActivity> {
+  int ballNumber = 1;
+
+  void randomiseBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          randomiseBall();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
